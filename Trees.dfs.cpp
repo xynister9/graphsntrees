ddabd7087a6821364ee0b0555967421b10f514af
k$hitiz <78069056+xynister9@users.noreply.github.com>
2022-08-29 20:38:08 +0530

    Create Trees.dfs.cpp

---
+++ Trees.dfs.cpp
@@ -0,0 +1,20 @@
+//(A) INPUT ADJACENCY LIST
+    for( int i=1; i<=n ;i++) adj[i].clear() , vis[i]=0 ;
+
+    for(int i=0 ;i<m; i++){
+        cin >> a >> b;
+        adj[a].push_back(b) , adj[b].push_back(a);
+    }
+
+//(B) DFS 
+vector<int> adj[MN];
+bool vis[MN] ;
+
+void dfs( int node ){
+    vis[node]=1;
+    //cout<<v<<"->";
+    for( auto child : adj[node]){
+        if( vis[child]==0)
+            dfs( child);
+    }
+}   
